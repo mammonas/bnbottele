7732554364ca2cf7ef93aa26b7910adff4fe6ab7
Khanh <daugauvuontre@gmail.com>
2021-03-07 22:43:02 +0700

    2nd

--- main.py
+++ main.py
@@ -7,23 +7,17 @@ from network.bn_call import BNCall
 tl = Timeloop()
 bn_call = BNCall()
 
-
-# @tl.job(interval=timedelta(seconds=5))
-# def execute_cron_job():
-#     bn_call.get_price(symbols='BNBUSDT')
-
-
 if __name__ == "__main__":
     syms = sys.argv[1]
     syms2 = sys.argv[2]
     token = sys.argv[3]
 
-    @tl.job(interval=timedelta(seconds=3600))
+    @tl.job(interval=timedelta(seconds=10))
     def execute_cron_job():
-        msg = bn_call.get_price(symbols=syms)
-        msg2 = bn_call.get_price(symbols=syms2)
+        bn_call.get_price(symbols=syms, token=token)
+        bn_call.get_price(symbols=syms2, token=token)
         # print(msg)
-        bn_call.send_message_to_bot(token=token, msg=msg)
-        bn_call.send_message_to_bot(token=token, msg=msg2)
+        # bn_call.send_message_to_bot(token=token, msg=msg)
+        # bn_call.send_message_to_bot(token=token, msg=msg2)
 
     tl.start(block=True)
--- network/bn_call.py
+++ network/bn_call.py
@@ -2,17 +2,23 @@ import requests
 import datetime
 import urllib.parse
 from colorama import Fore, Style
+
 GET_PRICE_ENDPOINT = 'https://binance.com/api/v3/ticker/24hr?symbol='
 TELE_END_POINT = 'https://api.telegram.org/bot'
 TELE_END_POINT_PARAMS = '/sendMessage?chat_id=-573320155&text='
 LAST_BUY_PRICE_BNB = 244.9068
 LAST_BUY_PRICE_BTC = 50800.00
 highest_perc = None
+is_first_send_bnb = False
+is_first_send_btc = False
+
 
 class BNCall:
 
-    def get_price(self, symbols):
+    def get_price(self, symbols, token):
         global highest_perc
+        global is_first_send_bnb
+        global is_first_send_btc
         end_point = GET_PRICE_ENDPOINT + symbols
         r = requests.get(end_point)
         time_str = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -22,19 +28,32 @@ class BNCall:
         diff = abs(last_sell_pr - price_current)
         percent_diff = (abs(diff) / last_sell_pr) * 100
         percent_diff_txt = '%s%.2f' % (dau, percent_diff)
-        percent_diff_txt_return = percent_diff_txt
 
-        if highest_perc is None:
-            highest_perc = percent_diff
-        elif percent_diff > highest_perc:
-            highest_perc = percent_diff
+        if symbols == 'BNBUSDT' and is_first_send_bnb is False:
+            is_first_send_bnb = True
+            msg = '%s-%s\t%.4f\t%s%%' % (time_str, symbols, price_current, percent_diff_txt)
+            self.send_message_to_bot(token, msg)
+        if symbols == 'BTCUSDT' and is_first_send_btc is False:
+            is_first_send_btc = True
+            msg = '%s-%s\t%.4f\t%s%%' % (time_str, symbols, price_current, percent_diff_txt)
+            self.send_message_to_bot(token, msg)
 
-        if highest_perc == percent_diff:
-            color = Fore.GREEN if dau == '+' else Fore.RED
-            percent_diff_txt = color + percent_diff_txt + Style.RESET_ALL
-        
+        if dau == '+':
+            msg = '%s-%s\t%.4f\t%s%%' % (time_str, symbols, price_current, percent_diff_txt)
+            self.send_message_to_bot(token, msg)
+
+        # if highest_perc is None:
+        #     highest_perc = percent_diff
+        #
+        # elif percent_diff > highest_perc:
+        #     highest_perc = percent_diff
+
+        # if highest_perc == percent_diff:
+        #     color = Fore.GREEN if dau == '+' else Fore.RED
+        # percent_diff_txt = color + percent_diff_txt + Style.RESET_ALL
+
         # print('%s Price:%.4f Diff:%s%.4f Per:%s' %(time_str, price_current, dau, diff, percent_diff_txt))
-        return '%s-%s\t%.4f\t%s%%' % (time_str, symbols, price_current, percent_diff_txt_return)
+        # return '%s-%s\t%.4f\t%s%%' % (time_str, symbols, price_current, percent_diff_txt_return)
 
     def send_message_to_bot(self, token, msg):
         encoded_msg = urllib.parse.quote(msg)
